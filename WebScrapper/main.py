225cf770a8c6270b259580190682320f3cfe8f39
Mario Beauchamp <bmar@bocdaz.ca>
2023-02-24 19:52:06 +0000

    Started moving files to create full self emconpassed application

--- WebScrapper/main.py
+++ WebScrapper/main.py
@@ -1,15 +1,15 @@
 from oil_market_scrapper import OilMarketsScrapper
-import data_slice as ds
+import DataManager.data_slice as ds
 
 
 if __name__ == "__main__":
-    # oms = OilMarketsScrapper(email="mariobeauchamp@bank-banque-canada.ca",password="LcVcSVi4e3bjTrcHjs2O")
-    # oms.batch_scrapper(
-    #     page_link="https://www.dailyoilbulletin.com/category/prices/",
-    #     json_name="prices",
-    #     next_page=True
-    # )    
-    # ds.split_by_paragraph("oil_category")
-    # ds.shuffle_json("prices")
-    # ds.shuffle_json("oil_category")
-    # ds.get_500("shuffle_paragraph_oil_category",start=3000,end=3500)
+    oms = OilMarketsScrapper(email="mariobeauchamp@bank-banque-canada.ca",password="LcVcSVi4e3bjTrcHjs2O")
+    oms.batch_scrapper(
+        page_link="https://www.dailyoilbulletin.com/category/prices/",
+        json_name="prices",
+        next_page=True
+    )    
+    ds.split_by_paragraph("oil_category")
+    ds.shuffle_json("prices")
+    ds.shuffle_json("oil_category")
+    ds.get_500("shuffle_paragraph_oil_category",start=3000,end=3500)
---
+++ main.py
@@ -0,0 +1,11 @@
+from WebScrapper.oil_market_scrapper import OilMarketsScrapper
+
+def main():
+    pass
+
+def get_todays_arcticles():
+    oms = OilMarketsScrapper(email="mariobeauchamp@bank-banque-canada.ca",password="LcVcSVi4e3bjTrcHjs2O")
+
+
+if __name__ == "__main__":
+    main()
